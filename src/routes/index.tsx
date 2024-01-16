9d217ac262bda8ea2c56e665a969519cd37c660e
Javi Carrion <code-vista@outlook.com>
2024-01-15 22:42:04 -0500

    chore(global): :lipstick: add fonts + add project name

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,22 +4,17 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <h1>We Who Care Site</h1>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "We Who Care",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "We Who Care site description",
     },
   ],
 };
